f3ac72a939a08cf7d18a4f22672334ff5643ce57
Khazoda <khazoda@gmail.com>
2024-06-24 16:57:08 +0100

    Initial Commit

---
+++ src/main/java/com/khazoda/basic_storage/BasicStorageDataGenerator.java
@@ -0,0 +1,11 @@
+package com.khazoda.basic_storage;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BasicStorageDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
