e82b748aeae6737859cfebfd74250f663ccfe7bc
June|Khazoda <khazoda@gmail.com>
2025-11-22 12:35:28 +0000

    - fix collective incompatibility - axe check is tag based instead of instanceof

--- src/main/java/com/khazoda/basicstorage/block/CrateBlock.java
+++ src/main/java/com/khazoda/basicstorage/block/CrateBlock.java
@@ -26,10 +26,10 @@ import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.pathing.NavigationType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.context.LootWorldContext;
+import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.sound.SoundCategory;
@@ -73,11 +73,11 @@ public class CrateBlock extends BlockWithEntity {
 
   private static Block.Settings getCrateSettings() {
     return Settings.create()
-        .sounds(BlockSoundGroup.WOOD)
-        .pistonBehavior(PistonBehavior.BLOCK)
-        .instrument(NoteBlockInstrument.BASS)
-        .mapColor(MapColor.OAK_TAN)
-        .strength(1f);
+            .sounds(BlockSoundGroup.WOOD)
+            .pistonBehavior(PistonBehavior.BLOCK)
+            .instrument(NoteBlockInstrument.BASS)
+            .mapColor(MapColor.OAK_TAN)
+            .strength(1f);
   }
 
   public CrateBlock(Settings settings) {
@@ -92,7 +92,7 @@ public class CrateBlock extends BlockWithEntity {
 
   @Override
   public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer,
-      ItemStack itemStack) {
+                       ItemStack itemStack) {
     super.onPlaced(world, pos, state, placer, itemStack);
     notifyNearbyStations(world, pos);
     world.emitGameEvent(placer, GameEvent.BLOCK_PLACE, pos);
@@ -100,12 +100,14 @@ public class CrateBlock extends BlockWithEntity {
 
   @Override
   protected float calcBlockBreakingDelta(BlockState state, PlayerEntity player, BlockView world, BlockPos pos) {
+    if (!player.canModifyBlocks()) return super.calcBlockBreakingDelta(state, player, world, pos);
     if (BasicStorageConfig.getInstance().breakWithAxeOnly()) {
-      ItemStack mainHandStack = player.getMainHandStack();
-      if (mainHandStack.getItem() instanceof AxeItem) {
+      boolean usingAxe = player.getMainHandStack().isIn(ItemTags.AXES);
+      if (usingAxe) {
         return super.calcBlockBreakingDelta(state, player, world, pos);
+      } else {
+        return 0.0f;
       }
-      return 0.0f;
     }
     return super.calcBlockBreakingDelta(state, player, world, pos);
   }
@@ -198,7 +200,7 @@ public class CrateBlock extends BlockWithEntity {
    * UseBlockCallback helper method
    **/
   private static int insertMaximum(PlayerEntity player, ItemStack playerStack, CrateSlot slot,
-      Transaction transaction) {
+                                   Transaction transaction) {
     /*
      * Insert as many items as possible from player's inventory if slot is empty, or
      * matches held stack
@@ -213,7 +215,7 @@ public class CrateBlock extends BlockWithEntity {
     } else {
       /* Insert into crate with items */
       return (int) StorageUtil.move(PlayerInventoryStorage.of(player), slot, itemVariant -> true, Integer.MAX_VALUE,
-          transaction);
+              transaction);
     }
   }
 
@@ -227,7 +229,7 @@ public class CrateBlock extends BlockWithEntity {
       message = Text.translatable("message.basicstorage.crate.empty").withColor(0xFFDD99);
     } else {
       message = Text.literal(NumberFormatter.toFormattedNumber(slot.getAmount()) + " "
-          + slot.getResource().getItem().getName().getString()).withColor(0xFFDD99);
+              + slot.getResource().getItem().getName().getString()).withColor(0xFFDD99);
     }
     player.sendMessage(message, true);
     return ActionResult.CONSUME;
@@ -250,7 +252,7 @@ public class CrateBlock extends BlockWithEntity {
       if (stack.isDamaged())
         return false;
       if (stack.isOf(BlockRegistry.CRATE_BLOCK.asItem())
-          && stack.contains(DataComponentRegistry.CRATE_CONTENTS))
+              && stack.contains(DataComponentRegistry.CRATE_CONTENTS))
         return false;
       if (!ItemVariant.of(stack).equals(slot.getResource()) && !slot.isBlank())
         return false;
@@ -282,7 +284,7 @@ public class CrateBlock extends BlockWithEntity {
 
       if (extracted == 1)
         world.playSound(null, pos, SoundRegistry.EXTRACT_ONE, SoundCategory.BLOCKS, 0.6f,
-            1.2f + ((-1 + random.nextFloat() * (1 + 1)) / 10));
+                1.2f + ((-1 + random.nextFloat() * (1 + 1)) / 10));
       if (extracted > 1)
         world.playSound(null, pos, SoundRegistry.EXTRACT_MANY, SoundCategory.BLOCKS, 0.75f, 1f);
       world.playSound(null, pos, SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.BLOCKS, 0.35f, 1f);
@@ -303,7 +305,7 @@ public class CrateBlock extends BlockWithEntity {
       CrateBlockEntity cbe = (CrateBlockEntity) be;
       if (!world.isClient() && player.isCreative() && !cbe.storage.getResource().toStack().isEmpty()) {
         getDroppedStacks(state, (ServerWorld) world, pos, cbe, player, player.getStackInHand(Hand.MAIN_HAND))
-            .forEach(stack -> ItemScatterer.spawn(world, pos.getX(), pos.getY(), pos.getZ(), stack));
+                .forEach(stack -> ItemScatterer.spawn(world, pos.getX(), pos.getY(), pos.getZ(), stack));
       }
     }
     return super.onBreak(world, pos, state, player);
@@ -356,8 +358,8 @@ public class CrateBlock extends BlockWithEntity {
     }
 
     return this.getDefaultState()
-        .with(Properties.ORIENTATION, Orientation.byDirections(facing, rotation))
-        .with(Properties.HORIZONTAL_FACING, legacyFacing); //Todo: remove after migration period
+            .with(Properties.ORIENTATION, Orientation.byDirections(facing, rotation))
+            .with(Properties.HORIZONTAL_FACING, legacyFacing); //Todo: remove after migration period
   }
 
   @Override
--- src/main/java/com/khazoda/basicstorage/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/com/khazoda/basicstorage/mixin/ServerPlayerInteractionManagerMixin.java
@@ -1,6 +1,7 @@
 package com.khazoda.basicstorage.mixin;
 
 import com.khazoda.basicstorage.block.CrateBlock;
+import net.minecraft.block.BlockState;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.network.ServerPlayerInteractionManager;
@@ -15,42 +16,43 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Mixin(ServerPlayerInteractionManager.class)
 public class ServerPlayerInteractionManagerMixin {
 
-  @Shadow protected ServerWorld world;
-  @Final
   @Shadow
+  protected ServerWorld world;
+  @Shadow
+  @Final
   protected ServerPlayerEntity player;
+  @Shadow
+  private int tickCounter;
 
-  @Unique private final Map<BlockPos, Integer> crateClickStartTicks = new HashMap<>();
+  @Unique
+  private int basicStorage$startTick = -1;
+  @Unique
+  private BlockPos basicStorage$targetPos = null;
 
   @Inject(method = "processBlockBreakingAction", at = @At("HEAD"))
   private void onCrateClickStart(BlockPos pos, PlayerActionC2SPacket.Action action, Direction direction, int worldHeight, int sequence, CallbackInfo ci) {
-    if (this.world.getBlockState(pos).getBlock() instanceof CrateBlock) {
-      if (action == PlayerActionC2SPacket.Action.START_DESTROY_BLOCK) {
-        crateClickStartTicks.put(pos, this.world.getServer().getTicks());
+    if (action == PlayerActionC2SPacket.Action.START_DESTROY_BLOCK) {
+      if (this.world.getBlockState(pos).getBlock() instanceof CrateBlock) {
+        this.basicStorage$startTick = this.tickCounter;
+        this.basicStorage$targetPos = pos;
+      } else {
+        this.basicStorage$targetPos = null;
       }
     }
-  }
-
-  @Inject(method = "processBlockBreakingAction", at = @At("TAIL"))
-  private void onCrateClickEnd(BlockPos pos, PlayerActionC2SPacket.Action action, Direction direction, int worldHeight, int sequence, CallbackInfo ci) {
-    if ((action == PlayerActionC2SPacket.Action.ABORT_DESTROY_BLOCK ||
-        action == PlayerActionC2SPacket.Action.STOP_DESTROY_BLOCK) &&
-        this.world.getBlockState(pos).getBlock() instanceof CrateBlock) {
-
-      Integer startTick = crateClickStartTicks.remove(pos);
-      if (startTick != null) {
-        int durationTicks = this.world.getServer().getTicks() - startTick;
-        // Quick Click (Extract)
-        if (durationTicks <= 3) {
-          CrateBlock.extractFromCrate(this.world, pos, this.player);
+    if (action == PlayerActionC2SPacket.Action.ABORT_DESTROY_BLOCK) {
+      if (this.basicStorage$targetPos != null && this.basicStorage$targetPos.equals(pos)) {
+        int duration = this.tickCounter - this.basicStorage$startTick;
+        BlockState state = this.world.getBlockState(pos);
+        if (state.getBlock() instanceof CrateBlock) {
+          if (duration <= 3) {
+            CrateBlock.extractFromCrate(this.world, pos, this.player);
+          }
         }
-        // Otherwise do nothing (allow breaking without extraction)
+        this.basicStorage$targetPos = null;
+        this.basicStorage$startTick = -1;
       }
     }
   }
